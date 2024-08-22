550b7ee83ca215aecf2a9d4d4a0c30f06d99a8d6
Anthony Benjamin <anthonyjbenjamin@gmail.com>
2024-08-22 10:00:51 -0700

    feat: ability to draw lines

---
+++ src/classes/Line.py
@@ -0,0 +1,18 @@
+from tkinter import Canvas
+from src.classes.Point import Point
+
+
+class Line:
+    def __init__(self, point_1: Point, point_2: Point) -> None:
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.point_1.x,
+            self.point_1.y,
+            self.point_2.x,
+            self.point_2.y,
+            fill=fill_color,
+            width=2,
+        )
--- src/classes/Window.py
+++ src/classes/Window.py
@@ -1,5 +1,7 @@
 from tkinter import Tk, BOTH, Canvas
 
+from src.classes.Line import Line
+
 
 class Window:
     def __init__(self, width: float, height: float, title: str) -> None:
@@ -19,6 +21,9 @@ class Window:
 
         while self.running:
             self.redraw()
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.canvas, fill_color)
 
     def close(self):
         self.running = False
